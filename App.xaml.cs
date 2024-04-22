0a33d37305c886d70856b640759e872f31945a18
Philippe Kurz <philippe.kurz@gmail.com>
2024-04-22 11:31:44 +0200

    Ajoutez des fichiers projet.

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HeraBougieCRUD
+{
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // Lire la configuration (dsn)
+        private static string _dsn = ConfigurationManager.ConnectionStrings["hbConnexionString"].ToString();
+        public static MySqlConnection _connexion = new MySqlConnection(_dsn); // connection
+
+    }
+
+}
---
+++ Vue/Articles.xaml.cs
@@ -0,0 +1,123 @@
+﻿
+
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HeraBougieCRUD.Vue
+{
+    public partial class Articles : UserControl
+    {
+        // Objet nécessaires pour SQL
+
+
+        private MySqlCommand _command; //requete 
+        private MySqlDataAdapter _adapter; // lire des donner et remplir et une table 
+
+        private DataTable _dt; // Ne fait pas partie de MySQL.Data (objet .NET)
+
+
+        public Articles()
+        {
+            //App._connexion.Open();
+            //App._connexion.Close();
+
+            InitializeComponent();
+
+            afficher();
+        }
+
+        //fonction prive renvoie rien 
+        private void afficher()
+        {
+
+            try
+            {
+                _adapter = new MySqlDataAdapter("SELECT * FROM articles;", App._connexion);  // instancier
+                _dt = new DataTable(); // instancie une table
+                App._connexion.Open();
+                _adapter.Fill(_dt); // on demande à l'adapteur de remplir la table ce qui trouve dans  _dt
+                dgArticle.ItemsSource = _dt.DefaultView; //transfere les donnees de _dt vers dgEtudiants
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                
+                _adapter.Dispose();
+                App._connexion.Close();
+                App._connexion.Dispose();
+
+            }
+        }
+
+
+        private void effacer()
+        {
+            txtId.Text = "";
+            txtNom.Text = "";
+            txtDescription.Text = "";
+            txtPrix.Text = "";
+            txtTaille.Text = "";
+            txtType.Text = "";
+            txtImage.Text = "";
+            txtId_Couleur.Text = "";
+            txtId_Cire.Text = "";
+            txtId_Parfum.Text = "";
+
+
+        }
+
+        private void btnAjouter_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("btnAjouter_Click");
+        }
+
+        private void btnModifier_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("btnModifier_Click");
+        }
+
+        private void btnSupprimer_Click(object sender, RoutedEventArgs e)
+        {
+            string _sql = "DELETE FROM produits WHERE id = @Id;";
+            _command = new MySqlCommand(_sql, App._connexion);
+            _command.Parameters.AddWithValue("@Id", txtId.Text);
+            App._connexion.Open();
+            _command.ExecuteNonQuery();
+            App._connexion.Close();
+
+            afficher();
+            effacer();
+        }
+
+        private void dgArticle_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if ((DataRowView)dgArticle.SelectedItem != null)
+            {
+                DataRowView _drv = (DataRowView)dgArticle.SelectedItem;
+
+                txtId.Text = _drv.Row["id"].ToString();
+                txtNom.Text = _drv.Row["nom"].ToString();
+                txtPrix.Text = _drv.Row["prix"].ToString();
+                txtType.Text = _drv.Row["type"].ToString();
+                txtTaille.Text = _drv.Row["taille"].ToString();
+                txtImage.Text = _drv.Row["image"].ToString();
+                txtDescription.Text = _drv.Row["description"].ToString();
+                txtId_Couleur.Text = _drv.Row["id_couleur"].ToString();
+                txtId_Cire.Text = _drv.Row["id_cire"].ToString();
+                txtId_Parfum.Text = _drv.Row["id_parfum"].ToString();
+            }
+
+        }
+
+    }
+}
+
+
+
+
